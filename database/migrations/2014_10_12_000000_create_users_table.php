d0e5f9e57861c1354f2dfd94457e1cabfaa7699e
BearRabbit324130 <bearrabbit324130@gmail.com>
2024-01-08 03:05:47 +0200

    update user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
+            $table->string('type')->default('student')->nullable();
+
+            //Entering Phone Number -----------------|
+            $table->string('phone_iso2', 3)->nullable();
+            $table->string('phone_dial_code', 7)->nullable();
+            $table->string('phone_number', 100)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
