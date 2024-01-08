1674f88ec6b5671d7296f4493780a779f671b5b9
BearRabbit324130 <bearrabbit324130@gmail.com>
2024-01-08 06:12:27 +0200

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,6 +17,72 @@
         </h2>
     </x-slot>
 
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+
+                        {{--todo--}}
+                        <div class="grid grid-cols-6 gap-4 mb-5">
+                            <div class="col-span-full">
+                                <div class="flex flex-wrap gap-4 justify-center">
+                                    <div class="dark:bg-gray-800 bg-white shadow rounded-xl flex gap-4 !p-4">
+                                        <div class="flex justify-center items-center">
+                                            <div class="text-8xl font-extrabold">
+                                                <p class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                                                    <i class="fa fa-trophy-alt"></i>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="flex flex-col gap-4">
+                                            <div>
+                                                <div class="text-6xl font-bold text-center">
+                                                    <p class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+                                                        32</p>
+                                                </div>
+                                            </div>
+                                            <div class="flex gap-4">
+                                                <div class="flex flex-col gap-4 text-center font-bold">
+                                                    <p class="dark:text-gray-400 text-gray-500 text-xs">Guitar</p>
+                                                    <span>44</span>
+                                                </div>
+                                                <div class="flex flex-col gap-4 text-center font-bold">
+                                                    <p class="dark:text-gray-400 text-gray-500 text-xs">Singing</p>
+                                                    <span>56</span>
+                                                </div>
+                                                <div class="flex flex-col gap-4 text-center font-bold">
+                                                    <p class="dark:text-gray-400 text-gray-500 text-xs">Drum</p>
+                                                    <span>20</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="dark:bg-gray-800 bg-white shadow rounded-xl flex gap-4 !p-4">
+                                        <div class="flex justify-center items-center">
+                                            <div class="text-8xl font-extrabold">
+                                                <p class="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-emerald-500">
+                                                    <i class="fa-thin fa-money-bill-trend-up"></i>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <div class="flex flex-col gap-4 items-center justify-center">
+                                            <div>
+                                                <div class="text-6xl font-bold text-center">
+                                                    <p class="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-emerald-500">
+                                                        8000 $</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
