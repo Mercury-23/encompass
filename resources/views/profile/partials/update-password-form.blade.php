69795b2c071f1a1818f9fced635676bc88220a3c
BearRabbit324130 <bearrabbit324130@gmail.com>
2024-01-25 15:56:51 +0200

    update password form blade

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -3,16 +3,13 @@
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('Update Password') }}
         </h2>
-
         <p class="mt-1 text-sm text-gray-600">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
     </header>
-
     <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('put')
-
         <div>
             <x-input-label for="update_password_current_password" :value="__('Current Password')" />
             <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
@@ -33,7 +30,6 @@
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
-
             @if (session('status') === 'password-updated')
                 <p
                     x-data="{ show: true }"
